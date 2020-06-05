b467c06ff8801ac714c350411ad803fcfa3e982f
chiaoooo <62134746+chiaoooo@users.noreply.github.com>
2020-06-05 17:30:38 +0800

    bolt

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody rBody;
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody>();
+        rBody.velocity = transform.forward * speed;
+    }
+}
